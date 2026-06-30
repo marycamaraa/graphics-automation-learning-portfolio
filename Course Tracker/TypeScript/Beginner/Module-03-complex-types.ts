ef4cab0703d5bfc676d8404e760c224e2762acf6
marycamaraa <mary.g.camara@bbc.co.uk>
2026-06-30 09:30:06 +0100

    update complex types module

---
+++ Course Tracker/TypeScript/Beginner/Module-03-complex-types.ts
@@ -0,0 +1,591 @@
+//! TypeScript Module: Complex Types
+
+//--------------------------------------------------------------------------------------
+// ==============================
+//*      ARRAYS | INTRODUCTION
+// ==============================
+
+/*
+Our TypeScript journey has now led us to a new destination: 
+ Array-bia. Typing arrays is a little bit different han working with primitive types.
+ This is because arrays usually contain many pieces of data.
+*/
+
+//! EXAMPLE 1
+// Keeping track of the array’s type means keeping track of every element’s type. For example:
+let firstArray = [1, 2, 3, 4];
+let secondArray = [5, "6", [7]];
+
+//NOTE: We can see that firstArray has elements that are all number types.
+//NOTE: On the other hand, secondArray has elements of varying types: number, string, and Array.
+//NOTE: Both are examples of valid JavaScript arrays.
+
+// Task 1
+// Manual type-checking
+
+let customersArray = [
+  "Custy Stomer",
+  "C. Oostomar",
+  "C.U.S. Tomer",
+  3432434,
+  "Custo Mer",
+  "Custopher Ustomer",
+  3432435,
+  "Kasti Yastimeur",
+];
+
+//Write Your Code here:
+function checkCustomersArray() {
+  for (let i = 0; i < customersArray.length; i++) {
+    if (typeof customersArray[i] != "string") {
+      console.log(`Type error: ${customersArray[i]} should be a string!`);
+    }
+  }
+}
+
+checkCustomersArray();
+
+function stringPush(val) {
+  if (typeof val === "string") {
+    customersArray.push(val);
+  }
+}
+
+//--------------------------------------------------------------------------------------
+// ==============================
+//*      ARRAY TYPE ANNOTATIONS
+// ==============================
+
+/*
+The TypeScript type annotation for array types is fairly straightforward: we put [] after the element type.
+
+*/
+
+//! EXAMPLE 1
+// In this code, names is an Array that can only contain strings:
+let names: string[] = ["Danny", "Samantha"];
+
+// An alternative method is to use the Array<T> syntax, where T stands for the type.
+let names1: Array<string> = ["Danny", "Samantha"];
+//NOTE: In the code above, the type, T, is string.
+
+// As we may expect, we get a type error if we try to assign an array of numbers to a string[] variable:
+let names2: string[] = [1, 2, 3]; // Type Error!
+
+// That was just like an assignment error with primitive types. TypeScript arrays, however, can also throw errors when elements of the wrong type are added:
+let names3: string[] = ["Damien"];
+names3.push(666); // Type Error!
+
+// Task 1
+// The code editor contains several array assignments. Add the appropriate type annotations to the array variables in the // Arrays: section.
+// Arrays:
+let bestNumbers: number[] = [7, 77, 4];
+let bestLunches: string[] = ["chicken soup", "non-chicken soup"];
+let bestBreakfasts: string[] = [
+  "scrambled eggs",
+  "oatmeal",
+  "tamago kake gohan",
+  "any kind of soup",
+];
+let bestBooleans: boolean[] = [true, false];
+
+//--------------------------------------------------------------------------------------
+// ==============================
+//*      MULTI-DIMMENTIONAL ARRAYS
+// ==============================
+
+/*
+We could also have arrays that only contain numbers (number[]) or booleans (boolean[]). 
+In fact, we can make arrays of any type whatsoever. We can also declare multidimensional arrays: arrays of arrays (of some type).
+
+*/
+
+//! EXAMPLE 1
+let arr: string[][] = [
+  ["str1", "str2"],
+  ["more", "strings"],
+];
+// Think of the string[][] above as short for (string[])[], that is, an array where every element has type string[].
+
+//! EXAMPLE 2
+// The empty array ([]) is compatible with any array type:
+let names4: string[] = []; // No type errors.
+let numbers: number[] = []; // No type errors.
+names4.push("Isabella");
+numbers.push(30);
+
+// Task 1
+// Please provide type annotations for all the arrays provided in the // Multidimensional arrays: section.
+
+// Multidimensional Arrays:
+let bestMealPlan: string[][] = [
+  bestLunches,
+  bestBreakfasts,
+  ["baked potato", "mashed potato"],
+];
+let bestBooleansTwice: boolean[][] = [bestBooleans, bestBooleans];
+let numbersMulti: number[][][] = [
+  [[1], [2, 3]],
+  [[7], bestNumbers],
+];
+
+//--------------------------------------------------------------------------------------
+// ==============================
+//*      TUPLES
+// ==============================
+
+/*
+As we know, JavaScript arrays are flexible and can have elements of different types. 
+- In TypeScript, when an array is typed with elements of specific types, it’s called a tuple.
+--- 
+As far as JavaScript is concerned, tuples act just like arrays. They both have .length properties. We can access (or change) the elements of both using [index]. But despite their similarities, tuples and arrays do not have compatible types within TypeScript. 
+*/
+
+//! EXAMPLE 1
+// With TypeScript, we can also define arrays with a fixed sequence of types:
+let ourTuple: [string, number, string, boolean] = [
+  "Is",
+  7,
+  "our favorite number?",
+  false,
+];
+//* The tuple above (ourTuple) contains the elements: 'Is', 7 , 'our favorite number?' , false and the tuple has a type of [string, number, string, boolean].
+
+//! EXAMPLE 2
+// Tuple types specify both the lengths and the orders of compatible tuples Docs In TypeScript, tuple types are defined as array-like containers with a fixed size and the types of each member element already set. and will cause an error if either of these conditions are not met:
+let numbersTuple: [number, number, number] = [1, 2, 3, 4]; // Type Error! numbersTuple should only have three elements.
+let mixedTuple: [number, string, boolean] = ["hi", 3, true]; // Type Error! The first elements should be a number, the second a string, and the third a boolean.
+
+//! EXAMPLE 3
+// Specifically, we can’t assign an array to a tuple variable, even when the elements are of the correct type:
+let tup: [string, string] = ["hi", "bye"];
+let arr: string[] = ["there", "there"];
+tup = ["there", "there"]; // No Errors.
+tup = arr; // Type Error! An array cannot be assigned to a tuple.
+
+//Task
+let favoriteCoordinates: [number, number, string, number, number, string] = [
+  40,
+  43.2,
+  "N",
+  73,
+  59.8,
+  "W",
+];
+
+favoriteCoordinates = [17, 45, "N", 142, 30, "E"];
+favoriteCoordinates[6] = -6.825; // error when you run tsc on terminal
+
+//--------------------------------------------------------------------------------------
+// ==============================
+//*      ARRAY TYPE INTERENCE
+// ==============================
+
+/*
+TypeScript can infer variable types from initial values and return statements. 
+
+
+*/
+
+//! EXAMPLE 1
+//  Even still, we may not know exactly what type inference to expect when dealing with array. For example
+let examAnswers = [true, false, false];
+//* What is the type of examAnswers? It seems it could equally well be boolean[] or [boolean, boolean, boolean]. In reality, it is always the first of these, since this is the less restrictive type.
+
+// This enables us to expand the array:
+examAnswers[3] = true; // No type error.
+
+//! EXAMPLE 2
+
+// Task
+// Don't change this part:
+let dogTup: [string, string, string, string] = [
+  "dog",
+  "brown fur",
+  "curly tail",
+  "sad eyes",
+];
+
+// Your code goes here:
+let myArr = dogTup.concat(["blue tail"]);
+
+// Your code goes here:
+myArr[50] = "not a dog";
+//--------------------------------------------------------------------------------------
+// ==============================
+//*     REST PARAMETER
+// ==============================
+
+/*
+Assigning types to rest parameters is similar to assigning types to arrays. 
+We can use the same syntax, either T[] or Array<T>, to specify the type of rest parameters.
+*/
+
+//! EXAMPLE 1
+function smush(firstString, ...otherStrings) {
+  let output = firstString;
+  for (let i = 0; i < otherStrings.length; i++) {
+    output = output.concat(otherStrings[i]);
+  }
+  return output;
+}
+
+/* Explain
+This function concatenates all of its arguments. 
+For example, calling: smush('hi ', 'there') returns the value 'hi there'.” 
+*/
+
+//The rest parameter otherStrings lets the function work with any number of parameters greater than zero:
+smush("a", "h", "h", "H", "H", "H", "!", "!"); // Returns: 'ahhHHH!!'.
+
+//! EXAMPLE 2
+/* 
+The function works well, but it is not type safe. We don’t want a misguided coder to make a mistake like smush(1,2,3), when that would cause an error. 
+TypeScript to the rescue! Type annotations for a rest parameter are identical to type annotations for arrays. 
+The function with a correctly typed rest parameter is then:
+*/
+function smush(firstString, ...otherStrings: string[]) {
+  /*rest of function*/
+}
+// With this change, TypeScript will treat otherStrings as an array of strings. This means that smush(1,2,3) will result in a type error because [2,3] is not an array of strings.
+
+// Task 1
+// The function addPower takes a number p and any number of additional numbers as parameters. It returns the sum of the p-th powers of the additional numbers.
+// For example, addPower(2, 3, 4) should return 25, since 3^2 + 4^2 = 25. Add type annotations to the parameters and return type of addPower to make it type safe.
+function addPower(p: number, ...numsToAdd: number[]): number {
+  let answer = 0;
+  for (let i = 0; i < numsToAdd.length; i++) {
+    answer += numsToAdd[i] ** p;
+  }
+  return answer;
+}
+
+addPower("a string", 4, 5, 6);
+
+//--------------------------------------------------------------------------------------
+// ==============================
+//*      SPREAD SYNTAX
+// ==============================
+
+/*
+Like the finest wines and cheeses, TypeScript’s tuples pair beautifully with JavaScript’s spread syntax
+
+*/
+
+//! EXAMPLE 1
+// This is most useful for function calls that use lots of arguments, like this:
+function gpsNavigate(
+  startLatitudeDegrees: number,
+  startLatitudeMinutes: number,
+  startNorthOrSouth: string,
+  startLongitudeDegrees: number,
+  startLongitudeMinutes: number,
+  startEastOrWest: string,
+  endLatitudeDegrees: number,
+  endLatitudeMinutes: number,
+  endNorthOrSouth: string,
+  endLongitudeDegrees: number,
+  endLongitudeMinutes: number,
+  endEastOrWest: string,
+) {
+  /* navigation subroutine here */
+}
+
+/* 
+The function call gpsNavigate(40, 43.2, 'N', 73, 59.8, 'W', 25, 0, 'N', 71, 0, 'W') 
+calculates a route from the Codecademy offices in New York City (40 degrees 43.2 minutes north, 73 degrees 59.8 minutes west) to selected coordinates in the Bermuda Triangle. 
+We all agree that this function call is awkward to read.
+*/
+
+// Instead, we can use tuple variables that represent the starting and ending coordinates:
+let codecademyCoordinates: [number, number, string, number, number, string] = [
+  40,
+  43.2,
+  "N",
+  73,
+  59.8,
+  "W",
+];
+let bermudaTCoordinates: [number, number, string, number, number, string] = [
+  25,
+  0,
+  "N",
+  71,
+  0,
+  "W",
+];
+
+//! EXAMPLE 2
+// These tuple annotations guarantee that the types of the elements will be valid function parameters for gpsNavigate().
+// Now, we use JavaScript’s spread syntax to write a very readable function call:
+
+gpsNavigate(...codecademyCoordinates, ...bermudaTCoordinates);
+// And by the way, this makes the return trip really convenient to compute too:
+gpsNavigate(...bermudaTCoordinates, ...codecademyCoordinates);
+// If there is a return trip . . .
+
+//* Task 1
+// Create a function that performs a dance move with the given parameters
+function performDanceMove(
+  moveName: string,
+  moveReps: number,
+  hasFlair: boolean,
+): void {
+  console.log(`I do the ${moveName} ${moveReps} times !`);
+  if (hasFlair) {
+    console.log("I do it with flair!");
+  }
+}
+
+let danceMoves: [string, number, boolean][] = [
+  ["chicken beak", 4, false],
+  ["wing flap", 4, false],
+  ["tail feather shake", 4, false],
+  ["clap", 4, false],
+  ["chicken beak", 4, true],
+  ["wing flap", 4, true],
+  ["tail feather shake", 4, true],
+  ["clap", 4, true],
+];
+
+danceMoves.forEach((move) => performDanceMove(...move));
+
+//--------------------------------------------------------------------------------------
+// ==============================
+//*      SECTION
+// ==============================
+
+/*
+
+*/
+
+//! EXAMPLE 1
+
+//! EXAMPLE 2
+
+//! EXAMPLE 3
+
+//--------------------------------------------------------------------------------------
+// ==============================
+//*      SECTION
+// ==============================
+
+/*
+
+*/
+
+//! EXAMPLE 1
+
+//! EXAMPLE 2
+
+//! EXAMPLE 3
+
+//--------------------------------------------------------------------------------------
+// ==============================
+//*      SECTION
+// ==============================
+
+/*
+
+*/
+
+//! EXAMPLE 1
+
+//! EXAMPLE 2
+
+//! EXAMPLE 3
+
+//--------------------------------------------------------------------------------------
+// ==============================
+//*      SECTION
+// ==============================
+
+/*
+
+*/
+
+//! EXAMPLE 1
+
+//! EXAMPLE 2
+
+//! EXAMPLE 3
+
+//--------------------------------------------------------------------------------------
+// ==============================
+//*      SECTION
+// ==============================
+
+/*
+
+*/
+
+//! EXAMPLE 1
+
+//! EXAMPLE 2
+
+//! EXAMPLE 3
+
+//--------------------------------------------------------------------------------------
+// ==============================
+//*      SECTION
+// ==============================
+
+/*
+
+*/
+
+//! EXAMPLE 1
+
+//! EXAMPLE 2
+
+//! EXAMPLE 3
+
+//--------------------------------------------------------------------------------------
+// ==============================
+//*      SECTION
+// ==============================
+
+/*
+
+*/
+
+//! EXAMPLE 1
+
+//! EXAMPLE 2
+
+//! EXAMPLE 3
+
+//--------------------------------------------------------------------------------------
+// ==============================
+//*      SECTION
+// ==============================
+
+/*
+
+*/
+
+//! EXAMPLE 1
+
+//! EXAMPLE 2
+
+//! EXAMPLE 3
+
+//--------------------------------------------------------------------------------------
+// ==============================
+//*      SECTION
+// ==============================
+
+/*
+
+*/
+
+//! EXAMPLE 1
+
+//! EXAMPLE 2
+
+//! EXAMPLE 3
+
+//--------------------------------------------------------------------------------------
+// ==============================
+//*      SECTION
+// ==============================
+
+/*
+
+*/
+
+//! EXAMPLE 1
+
+//! EXAMPLE 2
+
+//! EXAMPLE 3
+
+//--------------------------------------------------------------------------------------
+// ==============================
+//*      SECTION
+// ==============================
+
+/*
+
+*/
+
+//! EXAMPLE 1
+
+//! EXAMPLE 2
+
+//! EXAMPLE 3
+
+//--------------------------------------------------------------------------------------
+// ==============================
+//*      SECTION
+// ==============================
+
+/*
+
+*/
+
+//! EXAMPLE 1
+
+//! EXAMPLE 2
+
+//! EXAMPLE 3
+
+//--------------------------------------------------------------------------------------
+// ==============================
+//*      SECTION
+// ==============================
+
+/*
+
+*/
+
+//! EXAMPLE 1
+
+//! EXAMPLE 2
+
+//! EXAMPLE 3
+
+//--------------------------------------------------------------------------------------
+// ==============================
+//*      SECTION
+// ==============================
+
+/*
+
+*/
+
+//! EXAMPLE 1
+
+//! EXAMPLE 2
+
+//! EXAMPLE 3
+
+//--------------------------------------------------------------------------------------
+// ==============================
+//*      SECTION
+// ==============================
+
+/*
+
+*/
+
+//! EXAMPLE 1
+
+//! EXAMPLE 2
+
+//! EXAMPLE 3
+
+//--------------------------------------------------------------------------------------
+// ==============================
+//*      SECTION
+// ==============================
+
+/*
+
+*/
+
+//! EXAMPLE 1
+
+//! EXAMPLE 2
+
+//! EXAMPLE 3
